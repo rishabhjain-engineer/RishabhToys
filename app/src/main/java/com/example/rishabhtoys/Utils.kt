3a7f8d833fd7de1fb3bc530a671a8dce324c61cb
rishabh <rishabh5.jain@paytm.com>
2020-07-01 20:53:03 +0530

    access to db files and locally saved

--- app/src/main/java/com/example/rishabhtoys/Utils.kt
+++ app/src/main/java/com/example/rishabhtoys/Utils.kt
@@ -1,8 +1,10 @@
 package com.example.rishabhtoys
 
+import android.content.Context
+import android.os.Environment
+import android.util.Log
+import java.io.*
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import java.util.*
 
 class Utils {
@@ -13,5 +15,36 @@ class Utils {
             val formatter = SimpleDateFormat("dd.MM.yyyy",Locale.ENGLISH)
             return formatter.format(date)
         }
+
+
+        fun createBackup(context: Context){
+
+            // Get access to Db directory location and fetch all files in it
+            val data = Environment.getDataDirectory()
+            val currentDbPath = "//data//com.example.rishabhtoys//databases//"
+            val dbDirectory = File(data , currentDbPath)
+            val fileList = dbDirectory.listFiles()
+
+            //create a new directory, in internal storage.
+            val outputDirectory = File(context.filesDir , "Backup")
+            if(!outputDirectory.exists()){
+                outputDirectory.mkdir()
+            }
+
+            // Copy all files from Db directory to newly created directory
+            if(fileList.isNotEmpty()){
+                fileList?.forEach {
+                    val outputFilename = it.name
+                    val backupDir = File(outputDirectory,outputFilename)
+                    val src = FileInputStream(it).channel
+                    val destination = FileOutputStream(backupDir).channel
+                    destination.transferFrom(src, 0 , src.size())
+                    src.close()
+                    destination.close()
+                }
+
+            }
+        }
+
     }
 }
