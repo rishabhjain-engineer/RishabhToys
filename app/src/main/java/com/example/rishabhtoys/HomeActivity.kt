5b259d71eae3dceda13dc3fe7a7a8584a987ea2b
rishabh <rishabh5.jain@paytm.com>
2020-07-09 12:41:30 +0530

    frament added initially and backstack + menu item

--- app/src/main/java/com/example/rishabhtoys/HomeActivity.kt
+++ app/src/main/java/com/example/rishabhtoys/HomeActivity.kt
@@ -1,11 +1,11 @@
 package com.example.rishabhtoys
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_home.*
+
 
 class HomeActivity : BaseActivity() {
 
@@ -13,35 +13,125 @@ class HomeActivity : BaseActivity() {
     private val REPORT_FRAGMENT = "report_fragment"
     private val PROFILE_FRAGMENT = "profile_fragment"
 
+    private val homeFragmentObject: HomeFragment = HomeFragment()
+    private val reportFragmentObject: ReportFragment = ReportFragment()
+    private val profileFragmentObject: ProfileFragment = ProfileFragment()
+    private lateinit var menuItem: MenuItem
+
+
+    private lateinit var currentlyOpenFragment: Fragment
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         bottom_navigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener)
-        openFragment(HomeFragment(), HOME_FRAGMENT)
+        // openFragment(HomeFragment(), HOME_FRAGMENT)
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        openFragments()
     }
 
-    private fun openFragment(fragment:Fragment, tag:String){
+    /*private fun openFragment(fragment: Fragment, tag: String) {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.container,fragment,tag)
+        fragmentTransaction.replace(R.id.container, fragment, tag)
         fragmentTransaction.addToBackStack(tag)
         fragmentTransaction.commit()
+    }*/
+
+
+    private fun openFragments() {
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.container, profileFragmentObject, PROFILE_FRAGMENT)
+            .hide(profileFragmentObject)
+            .addToBackStack(PROFILE_FRAGMENT)
+            .commit()
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.container, reportFragmentObject, REPORT_FRAGMENT)
+            .hide(reportFragmentObject)
+            .addToBackStack(REPORT_FRAGMENT)
+            .commit()
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.container, homeFragmentObject, HOME_FRAGMENT)
+            .show(homeFragmentObject)
+            .addToBackStack(HOME_FRAGMENT)
+            .commit()
+
+        currentlyOpenFragment = homeFragmentObject
     }
 
-    private val navigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
-        when(it.itemId){
-            R.id.navigation_home -> {
-                openFragment(HomeFragment(),HOME_FRAGMENT)
-                return@OnNavigationItemSelectedListener true
+
+    private val navigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener {
+
+            menuItem = it
+
+            when (it.itemId) {
+                R.id.navigation_home -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .hide(currentlyOpenFragment)
+                        .show(homeFragmentObject)
+                        .commit()
+                    currentlyOpenFragment = homeFragmentObject
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_report -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .hide(currentlyOpenFragment)
+                        .show(reportFragmentObject)
+                        .commit()
+                    currentlyOpenFragment = reportFragmentObject
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_profile -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .hide(currentlyOpenFragment)
+                        .show(profileFragmentObject)
+                        .commit()
+                    currentlyOpenFragment = profileFragmentObject
+                    return@OnNavigationItemSelectedListener true
+                }
+                else -> return@OnNavigationItemSelectedListener false
             }
-            R.id.navigation_report -> {
-                openFragment(ReportFragment(),REPORT_FRAGMENT)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_profile -> {
-                openFragment(ProfileFragment(),PROFILE_FRAGMENT)
-                return@OnNavigationItemSelectedListener true
-            }
-            else -> return@OnNavigationItemSelectedListener false
         }
+
+    override fun onBackPressed() {
+
+        when (currentlyOpenFragment) {
+            homeFragmentObject -> {
+                finish()
+            }
+
+            reportFragmentObject -> {
+                supportFragmentManager
+                    .beginTransaction()
+                    .hide(reportFragmentObject)
+                    .show(homeFragmentObject)
+                    .commit()
+
+                currentlyOpenFragment = homeFragmentObject
+                bottomNavigationView.selectedItemId = R.id.navigation_home
+            }
+
+            profileFragmentObject -> {
+                supportFragmentManager
+                    .beginTransaction()
+                    .hide(profileFragmentObject)
+                    .show(homeFragmentObject)
+                    .commit()
+
+                currentlyOpenFragment = homeFragmentObject
+                bottomNavigationView.selectedItemId = R.id.navigation_home
+            }
+        }
+
     }
 }
