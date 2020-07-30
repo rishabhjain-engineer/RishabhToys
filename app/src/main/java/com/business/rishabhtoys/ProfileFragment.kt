96addc9871a1f403244a8587fbe2719f1e9e390f
rishabh <rishabh5.jain@paytm.com>
2020-07-30 17:27:41 +0530

    google drive folder/files

--- app/src/main/java/com/business/rishabhtoys/ProfileFragment.kt
+++ app/src/main/java/com/business/rishabhtoys/ProfileFragment.kt
@@ -5,10 +5,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -24,9 +24,6 @@ import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.io.FileInputStream
-import java.io.FileOutputStream
 import java.util.*
 
 /**
@@ -48,26 +45,25 @@ class ProfileFragment : Fragment() {
 
         create_backup_btn.setOnClickListener {
             googleSignIn()
-            //Utils.createBackup(activity!!)
         }
     }
 
-    private fun googleSignIn(){
+    private fun googleSignIn() {
         val signIn = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .requestScopes(Scope(DriveScopes.DRIVE_FILE))
             .build()
 
-        val client : GoogleSignInClient = GoogleSignIn.getClient(activity!!,signIn)
-        startActivityForResult(client.signInIntent , 400)
+        val client: GoogleSignInClient = GoogleSignIn.getClient(activity!!, signIn)
+        startActivityForResult(client.signInIntent, 400)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        when(requestCode) {
+        when (requestCode) {
             400 -> {
-                if(resultCode == Activity.RESULT_OK){
+                if (resultCode == Activity.RESULT_OK) {
                     Log.e("Rishabh", "handle sign in")
                     handleSignIn(data)
                 }
@@ -82,36 +78,19 @@ class ProfileFragment : Fragment() {
             .addOnSuccessListener {
 
                 Log.e("Rishabh", "sign in success")
-                val credentials : GoogleAccountCredential = GoogleAccountCredential
+                val credentials: GoogleAccountCredential = GoogleAccountCredential
                     .usingOAuth2(activity!!, Collections.singleton(DriveScopes.DRIVE_FILE))
 
                 credentials.selectedAccount = it.account
 
-                val driveService : Drive = Drive.Builder(
+                val driveService: Drive = Drive.Builder(
                     AndroidHttp.newCompatibleTransport(),
                     GsonFactory(),
-                    credentials)
+                    credentials
+                )
                     .setApplicationName("Rishabh Toy's Drive")
                     .build()
 
-
-              /*  // Upload your files to drive
-
-
-                val fileMetadata = File()
-                fileMetadata.name = "Invoices1234"
-                fileMetadata.mimeType = "application/vnd.google-apps.folder"
-                // application/vnd.sqlite3, application/x-sqlite3
-
-
-                GlobalScope.launch (Dispatchers.IO){
-                    val file: File =
-                        driveService.files().create(fileMetadata)
-                            .setFields("id")
-                            .execute()
-                }
-
-*/
 
                 // Get access to Db directory location and fetch all files in it
                 val data = Environment.getDataDirectory()
@@ -119,35 +98,41 @@ class ProfileFragment : Fragment() {
                 val dbDirectory = java.io.File(data, currentDbPath)
                 val fileList = dbDirectory.listFiles()
 
-                Log.e("Rishabh","file list size: "+fileList.size)
-                GlobalScope.launch (Dispatchers.IO){
-                    // Upload each file
-                    if (fileList.isNotEmpty()) {
-                        fileList?.forEach {
 
-                            Log.e("Rishabh","file name: "+it.name)
+
+                GlobalScope.launch(Dispatchers.IO) {
+
+
+                    // Create backup folder
+
+                    val folderMetadata = File()
+                    folderMetadata.name = Utils.getCurrentDateString()
+                    folderMetadata.mimeType = "application/vnd.google-apps.folder"
+
+                    val folder: File =
+                        driveService.files().create(folderMetadata)
+                            .setFields("id")
+                            .execute()
+
+                    if (fileList.isNotEmpty()) {
+
+                        fileList?.forEach {
 
                             val fileMetadata = File()
                             fileMetadata.name = it.name
+                            fileMetadata.parents = mutableListOf<String>(folder.id)
 
                             val filePath: java.io.File = java.io.File(it.path)
-                            val fileContent : FileContent = FileContent("application/vnd.sqlite3",filePath)
+                            val fileContent: FileContent =
+                                FileContent("application/vnd.sqlite3", filePath)
 
                             val file: File =
-                                driveService.files().create(fileMetadata , fileContent)
+                                driveService.files().create(fileMetadata, fileContent)
                                     .setFields("id")
                                     .execute()
-
-                            withContext(Dispatchers.Main){
-                                System.out.println("File ID: " + file.getId());
-                                Log.e("Rishabh","file id: "+file.id)
-                            }
                         }
-
                     }
                 }
-
-
             }
     }
 
