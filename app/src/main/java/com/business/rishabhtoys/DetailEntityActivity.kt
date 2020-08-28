18cb6e87fd6128bdf24d26fc2a2a7ae466f9b94d
rishabh <rishabh5.jain@paytm.com>
2020-08-28 15:38:26 +0530

    save pdf format

--- app/src/main/java/com/business/rishabhtoys/DetailEntityActivity.kt
+++ app/src/main/java/com/business/rishabhtoys/DetailEntityActivity.kt
@@ -1,12 +1,19 @@
 package com.business.rishabhtoys
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.graphics.pdf.PdfDocument
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_detail_entity.*
+import java.io.File
+import java.io.FileOutputStream
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
@@ -21,6 +28,7 @@ class DetailEntityActivity : BaseActivity() {
     private lateinit var mLayoutManager: LinearLayoutManager
     private lateinit var mAdapter: TxnHistoryAdapter
     private var mTxnHistoryList : MutableList<TxnHistoryEntity> = ArrayList()
+    private var queryDataList:ArrayList<DetailInfoForEntity> = ArrayList<DetailInfoForEntity>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,9 +60,17 @@ class DetailEntityActivity : BaseActivity() {
             startActivity(intent)
         }
 
+        save_pdf_btn.setOnClickListener {
+            savePdf()
+        }
+
         mViewModel.getData()?.observe(this, Observer<List<DetailInfoForEntity>> {
 
             if (it.isNotEmpty()) {
+
+                // saving data
+                queryDataList = it as ArrayList<DetailInfoForEntity>
+
                 d_e_company_name.text = it[0].entity.companyName
                 d_e_company_address.text = it[0].entity.companyAddress
                 d_e_gst.text = it[0].entity.gstNo
@@ -92,5 +108,103 @@ class DetailEntityActivity : BaseActivity() {
             }
 
         })
+    }
+
+    private fun savePdf() {
+
+        var yCoordinate : Float = 0f
+        val pdfDocument = PdfDocument()
+        val paint = Paint()
+
+        val pageInfo = PdfDocument.PageInfo.Builder(400,600,1).create()
+        val firstPage = pdfDocument.startPage(pageInfo)
+
+        val canvas = firstPage.canvas
+
+        paint.textAlign = Paint.Align.CENTER
+        paint.color = Color.RED
+        paint.textSize = 16f
+        yCoordinate = 30f;
+        paint.typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD);
+        canvas.drawText( getString(R.string.rishabh_toys) , (pageInfo.pageWidth/2).toFloat(), 30f,paint)
+
+        paint.typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
+        paint.color = Color.BLACK
+        paint.textSize = 10f
+        canvas.drawText( getString(R.string.rishabh_toys_address) , (pageInfo.pageWidth/2).toFloat(), 45f,paint)
+        canvas.drawText( getString(R.string.rishabh_toys_gst) , (pageInfo.pageWidth/2).toFloat(), 60f,paint)
+        canvas.drawText( getString(R.string.rishabh_toys_contact) , (pageInfo.pageWidth/2).toFloat(), 75f,paint)
+        canvas.drawText( getString(R.string.rishabh_toys_email) , (pageInfo.pageWidth/2).toFloat(), 90f,paint)
+        canvas.drawLine(10f , 105f , (pageInfo.pageWidth-10).toFloat(), 105f , paint)
+
+
+        paint.style = Paint.Style.STROKE
+        paint.color = Color.BLACK
+        paint.strokeWidth = 3f
+        canvas.drawRect(20f, 120f , (pageInfo.pageWidth-20).toFloat(), 225f,paint)
+
+        paint.style = Paint.Style.FILL
+        paint.textAlign = Paint.Align.LEFT
+        paint.color = Color.BLACK
+        paint.textSize = 14f
+        canvas.drawText(getString(R.string.company_pdf_label),30f, 140f, paint)
+        canvas.drawText(getString(R.string.address_pdf_label),30f, 165f, paint)
+        canvas.drawText(getString(R.string.gst_pdf_label),30f, 190f, paint)
+        canvas.drawText(getString(R.string.contact_pdf_label),30f, 215f, paint)
+
+        canvas.drawLine(110f , 120f , 110f , 225f , paint)
+
+        canvas.drawText(queryDataList[0].entity.companyName,120f, 140f, paint)
+        canvas.drawText(queryDataList[0].entity.companyAddress,120f, 165f, paint)
+        canvas.drawText(queryDataList[0].entity.gstNo,120f, 190f, paint)
+        canvas.drawText(queryDataList[0].entity.primaryContactNo,120f, 215f, paint)
+
+        paint.color = Color.parseColor("#eba832")
+        canvas.drawRect(0f, 240f , (pageInfo.pageWidth).toFloat(), 280f,paint)
+
+        paint.color = Color.WHITE
+        paint.textAlign = Paint.Align.CENTER
+        paint.textSize = 16f
+        paint.typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD);
+        canvas.drawText(getString(R.string.transaction_history),(pageInfo.pageWidth/2).toFloat(), 265f, paint)
+
+        paint.color = Color.BLACK
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 1f
+        canvas.drawLine(0f,281f, pageInfo.pageWidth.toFloat(), 281f,paint)
+        canvas.drawLine(0f,311f, pageInfo.pageWidth.toFloat(), 311f,paint)
+
+        //Txn History Vertical Line
+        canvas.drawLine(130f,281f, 130f, pageInfo.pageHeight.toFloat(),paint)
+        canvas.drawLine(265f,281f, 265f, pageInfo.pageHeight.toFloat(),paint)
+
+
+        paint.style = Paint.Style.FILL
+        paint.textSize = 14f
+        paint.typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
+        paint.textAlign = Paint.Align.CENTER
+
+
+        canvas.drawText(getString(R.string.date_pdf_label) , 62f, 301f,  paint)
+        canvas.drawText(getString(R.string.amount_pdf_label), 194f, 301f,  paint)
+        canvas.drawText(getString(R.string.balance_pdf_label) , 326f, 301f,  paint)
+
+        //var yCoordinate = 331f
+        mTxnHistoryList.forEach {
+            canvas.drawText(Utils.getStringFromDate(it.date) , 62f, yCoordinate,  paint)
+            canvas.drawText(it.txnAmount.toString() , 194f, yCoordinate,  paint)
+            canvas.drawText(it.txnAmount.toString() , 326f, yCoordinate,  paint)
+
+            yCoordinate += 25f
+        }
+
+        pdfDocument.finishPage(firstPage)
+
+        val file = File(getExternalFilesDir(null), Utils.removeSpaceFromString(queryDataList[0].entity.companyName).plus("_").plus(Utils.getCurrentDateString()).plus(".pdf"))
+
+        Log.e("Rishabh","file path: "+file.path)
+
+        pdfDocument.writeTo(FileOutputStream(file))
+        pdfDocument.close()
     }
 }
--- app/src/main/java/com/business/rishabhtoys/Utils.kt
+++ app/src/main/java/com/business/rishabhtoys/Utils.kt
@@ -1,6 +1,10 @@
 package com.business.rishabhtoys
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.graphics.pdf.PdfDocument
 import android.os.Environment
 import java.io.File
 import java.io.FileInputStream
@@ -62,6 +66,121 @@ class Utils {
         fun displayFormattedAmount(amount: Float?): String {
             val df = DecimalFormat("##,##,###.##")
             return df.format(amount)
+        }
+
+        private fun createPdf(context: Context) {
+
+
+            val pdfDocument = PdfDocument()
+            val paint = Paint()
+
+            val pageInfo = PdfDocument.PageInfo.Builder(400,600,1).create()
+            val firstPage = pdfDocument.startPage(pageInfo)
+
+            val canvas = firstPage.canvas
+
+            paint.textAlign = Paint.Align.CENTER
+            paint.color = Color.RED
+            paint.textSize = 16f
+            paint.typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD);
+            canvas.drawText( "RISHABH TOYS" , (pageInfo.pageWidth/2).toFloat(), 30f,paint)
+
+            paint.typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
+            paint.color = Color.BLACK
+            paint.textSize = 10f
+            canvas.drawText( "Shop No. 2895/3, IIIrd floor, Janta Market, Sadar Bazar, Delhi-110006" , (pageInfo.pageWidth/2).toFloat(), 45f,paint)
+            canvas.drawText( "GST no: 07ACHPJ9322R1ZZ" , (pageInfo.pageWidth/2).toFloat(), 60f,paint)
+            canvas.drawText( "Mob no: +91-9868676991 | +91-7827820919" , (pageInfo.pageWidth/2).toFloat(), 75f,paint)
+            canvas.drawText( "Email: djain986867@gmail.com" , (pageInfo.pageWidth/2).toFloat(), 90f,paint)
+            canvas.drawLine(10f , 105f , (pageInfo.pageWidth-10).toFloat(), 105f , paint)
+
+
+            paint.style = Paint.Style.STROKE
+            paint.color = Color.BLACK
+            paint.strokeWidth = 3f
+            canvas.drawRect(20f, 120f , (pageInfo.pageWidth-20).toFloat(), 225f,paint)
+
+            paint.style = Paint.Style.FILL
+            paint.textAlign = Paint.Align.LEFT
+            paint.color = Color.BLACK
+            paint.textSize = 14f
+            canvas.drawText("Company",30f, 140f, paint)
+            canvas.drawText("Address",30f, 165f, paint)
+            canvas.drawText("GST No.",30f, 190f, paint)
+            canvas.drawText("Contact No.",30f, 215f, paint)
+
+            canvas.drawLine(110f , 120f , 110f , 225f , paint)
+
+            canvas.drawText("Hari Om Traders",120f, 140f, paint)
+            canvas.drawText("kalupur, Ahemdabad, Gujrat",120f, 165f, paint)
+            canvas.drawText("07UC3PJ4322R1VZ",120f, 190f, paint)
+            canvas.drawText("9654239943",120f, 215f, paint)
+
+            paint.color = Color.parseColor("#eba832")
+            canvas.drawRect(0f, 240f , (pageInfo.pageWidth).toFloat(), 280f,paint)
+
+            paint.color = Color.WHITE
+            paint.textAlign = Paint.Align.CENTER
+            paint.textSize = 16f
+            paint.typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD);
+            canvas.drawText("Transaction History",(pageInfo.pageWidth/2).toFloat(), 265f, paint)
+
+            paint.color = Color.BLACK
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = 1f
+            canvas.drawLine(0f,281f, pageInfo.pageWidth.toFloat(), 281f,paint)
+            canvas.drawLine(0f,311f, pageInfo.pageWidth.toFloat(), 311f,paint)
+
+            //Txn History Vertical Line
+            canvas.drawLine(130f,281f, 130f, pageInfo.pageHeight.toFloat(),paint)
+            canvas.drawLine(265f,281f, 265f, pageInfo.pageHeight.toFloat(),paint)
+
+
+            paint.style = Paint.Style.FILL
+            paint.textSize = 14f
+            paint.typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
+            paint.textAlign = Paint.Align.CENTER
+
+
+            canvas.drawText("Txn Date" , 62f, 301f,  paint)
+            canvas.drawText("Txn Amount" , 194f, 301f,  paint)
+            canvas.drawText("Balance" , 326f, 301f,  paint)
+
+
+            pdfDocument.finishPage(firstPage)
+
+            val file = File(context.getExternalFilesDir(null), "/RishabhToys.pdf" )
+
+            pdfDocument.writeTo(FileOutputStream(file))
+            pdfDocument.close()
+
+        }
+
+        fun removeSpaceFromString(data:String): String{
+            var modifiedData = data
+            if(data.contains(" ")){
+                modifiedData = data.replace(" ","")
+            }
+            return modifiedData
+        }
+
+        fun getPdfAddressFields(address:String) : Array<String>{
+
+            var firstField:String
+            val secondField :String
+
+            // character at 36th position is space, simply split and return
+            if(" " == address[36].toString()){
+                firstField = address.substring(0,36)
+                secondField = address.substring(36,address.length)
+            }else {
+                firstField = address.substring(0,36)
+                val lastSpaceIndex = firstField.lastIndexOf(" ")
+                firstField = firstField.substring(0,lastSpaceIndex)
+                secondField = address.substring(lastSpaceIndex,address.length)
+            }
+
+            return arrayOf(firstField,secondField)
         }
 
     }
